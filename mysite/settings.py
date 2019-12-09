fb8a39eb1871fbd30880d5b470602f216e6b3a05
Bruno Santos <brunosantos@protonmail.com>
2019-12-09 20:49:54 -0300

    Activating models

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
